63aecba53e6eaa077923e408114add23eb12377a
unproZhai <42622194+unproZhai@users.noreply.github.com>
2019-10-19 16:43:19 -0400

    updated better version

--- abs_so_maximum_quantity/model/product_template.py
+++ abs_so_maximum_quantity/model/product_template.py
@@ -1,27 +1,15 @@
 # -*- coding: utf-8 -*-
-#################################################################################
-#
-#    Odoo, Open Source Management Solution
-#    Copyright (C) 2019-today Ascetic Business Solution <www.asceticbs.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#################################################################################
 
 from odoo import api,fields,models,_
 
 class ProductTemplate(models.Model):
     _inherit="product.template"
 
-    maximum_order_quantity = fields.Float(string='Maximum Order Quantity',help="This field display maximum order qunatity",default=1000000000.00)
+    is_maximum = fields.Boolean(
+        string="Is Product Limited",
+        help="Check this field if this product has a maximum limit.")
+
+    maximum_order_quantity = fields.Float(
+    	string='Maximum Order Quantity',
+    	help="This field display maximum ordereds qunatity.")
+
